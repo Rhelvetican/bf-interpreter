3bf6f6a290144d325d61ec2e6fed83db3a88186e
Rh4096 <bionicvnb@gmail.com>
2023-12-28 05:19:54 +0700

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone)]
 pub enum Token {
     INC,
     DEC,
@@ -9,19 +10,104 @@ pub enum Token {
     LPE,
 }
 
-impl Token {
-    pub fn from_char(c: char) -> Option<Token> {
+#[derive(Debug, Clone)]
+pub enum Instruction {
+    INC,
+    DEC,
+    FWD,
+    BWD,
+    INP,
+    OUT,
+    LOOP(Vec<Instruction>),
+}
+
+pub fn lexer(src: String) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    for c in src.chars() {
         match c {
-            '+' => Some(Token::INC),
-            '-' => Some(Token::DEC),
-            '>' => Some(Token::FWD),
-            '<' => Some(Token::BWD),
-            ',' => Some(Token::INP),
-            '.' => Some(Token::OUT),
-            '[' => Some(Token::LPS),
-            ']' => Some(Token::LPE),
-            _ => None,
+            '+' => tokens.push(Token::INC),
+            '-' => tokens.push(Token::DEC),
+            '>' => tokens.push(Token::FWD),
+            '<' => tokens.push(Token::BWD),
+            ',' => tokens.push(Token::INP),
+            '.' => tokens.push(Token::OUT),
+            '[' => tokens.push(Token::LPS),
+            ']' => tokens.push(Token::LPE),
+            _ => (),
         }
     }
-    
+    tokens
+}
+
+pub fn parser(tokens: Vec<Token>) -> Vec<Instruction> {
+    let mut instructions: Vec<Instruction> = Vec::new();
+    let mut loop_stack = 0;
+    let mut loop_start = 0;
+
+    for (i, token) in tokens.iter().enumerate() {
+        if loop_stack == 0 {
+            let ins = match token {
+                Token::INC => Some(Instruction::INC),
+                Token::DEC => Some(Instruction::DEC),
+                Token::FWD => Some(Instruction::FWD),
+                Token::BWD => Some(Instruction::BWD),
+                Token::INP => Some(Instruction::INP),
+                Token::OUT => Some(Instruction::OUT),
+
+                Token::LPS => {
+                    loop_stack += 1;
+                    loop_start = i;
+                    None
+                }
+
+                Token::LPE => panic!("Unmatched loop end at {}", i),
+            };
+
+            match ins {
+                Some(ins) => instructions.push(ins),
+                None => (),
+            }
+        } else {
+            match token {
+                Token::LPS => loop_stack += 1,
+                Token::LPE => {
+                    loop_stack -= 1;
+                    if loop_stack == 0 {
+                        instructions.push(Instruction::LOOP(parser(
+                            tokens[loop_start + 1..i].to_vec(),
+                        )));
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    if loop_stack != 0 {
+        panic!("Unmatched loop start at {}", loop_start);
+    }
+
+    instructions
+}
+
+pub fn executor(ins: &Vec<Instruction>, tape: &mut Vec<i32>, ptr: &mut usize) {
+    for i in ins {
+        match i {
+            Instruction::INC => tape[*ptr] += 1,
+            Instruction::DEC => tape[*ptr] -= 1,
+            Instruction::FWD => *ptr += 1,
+            Instruction::BWD => *ptr -= 1,
+            Instruction::INP => {
+                let mut buf = String::new();
+                std::io::stdin().read_line(&mut buf).unwrap();
+                tape[*ptr] = buf.parse::<i32>().unwrap();
+            }
+            Instruction::OUT => print!("{}", tape[*ptr] as u8 as char),
+            Instruction::LOOP(ins) => {
+                while tape[*ptr] != 0 {
+                    executor(ins, tape, ptr);
+                }
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bf_interpreter::*;
+use std::{env, fs::{self, File}, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    
+    if args.len() != 2 {
+        println!("Usage: bf <file.bf>");
+        std::process::exit(1);
+    }
+
+    let filename = &args[1];
+    let mut file = File::open(filename).expect("File not found");
+    let mut src = String::new();
+    file.read_to_string(&mut src).expect("Could not read file");
+
+    let tokens = lexer(src);
+
+    let ins = parser(tokens);
+
+    //Setup the environment
+    let mut tape: Vec<i32> = vec![0; 32767];
+    let mut ptr: usize = 0;
+
+    executor(&ins, &mut tape, &mut ptr);
 }
